64fc7e76e220f6dcff2aacd1817a67b6749eb721
NiklasPirinen <bnipi003@edu.xamk.fi>
2020-11-25 14:41:20 +0200

    sixth commit

---
+++ Assets/Scripts/mute.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mute : MonoBehaviour
+{
+    public AudioSource bgm;
+    public bool muted;
+    public Sprite mutedS;
+    public Sprite unmutedS;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (muted)
+        {
+            bgm.volume = 0;
+            gameObject.GetComponent<SpriteRenderer>().sprite = mutedS;
+        }
+        if (!muted)
+        {
+            bgm.volume = 0.3f;
+            gameObject.GetComponent<SpriteRenderer>().sprite = unmutedS;
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            muted = !muted;
+        }
+    }
+}
